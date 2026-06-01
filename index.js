076a49645b04f5bba947b73a8195c26c59841ed3
Yuna Won <wonyoona31@gmail.com>
2026-06-01 09:20:41 +0900

    debounce

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const search = document.getElementById("search");
 const suggestions = document.getElementById("suggestions");
 const repoList = document.getElementById("repo-list");
 
-search.addEventListener("input", () => {
-  console.log(search.value);
-});
+function debounce(fn, delay) {
+  let timer;
+
+  return function (...args) {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
 
 async function getRepos(query) {
   const response = await fetch(
-    `https://api.github.com/search/repositories?q=${query}`,
+    `https://api.github.com/search/repositories?q=${query}`
   );
 
   const data = await response.json();
+
   return data.items || [];
 }
 
-search.addEventListener("input", async () => {
+async function handleInput() {
   const query = search.value.trim();
 
   if (query === "") {
@@ -24,11 +33,10 @@ search.addEventListener("input", async () => {
     return;
   }
 
-  suggestions.style.display = "block";
-
   const repos = await getRepos(query);
 
   suggestions.innerHTML = "";
+  suggestions.style.display = "block";
 
   repos.slice(0, 5).forEach((repo) => {
     const userLi = document.createElement("li");
@@ -36,27 +44,36 @@ search.addEventListener("input", async () => {
     userLi.classList.add("suggestion");
     userLi.textContent = repo.name;
 
-    suggestions.append(userLi);
-
     userLi.addEventListener("click", () => {
       const card = document.createElement("div");
 
       card.classList.add("repo");
 
       card.innerHTML = `
-    Name: ${repo.name}<br>
-    Owner: ${repo.owner.login}<br>
-    Stars: ${repo.stargazers_count}
-    <span class ="delete">X</span>`;
+        Name: ${repo.name}<br>
+        Owner: ${repo.owner.login}<br>
+        Stars: ${repo.stargazers_count}
+        <span class="delete">X</span>
+      `;
 
       const deleteBtn = card.querySelector(".delete");
+
       deleteBtn.addEventListener("click", () => {
         card.remove();
       });
+
       repoList.append(card);
+
       search.value = "";
       suggestions.innerHTML = "";
       suggestions.style.display = "none";
     });
+
+    suggestions.append(userLi);
   });
-});
+}
+
+search.addEventListener(
+  "input",
+  debounce(handleInput, 500)
+);
